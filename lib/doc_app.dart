1ff03022753cd1f672feee3f8277123b3622c628
Kerollosmm <kerollosmilad21@gmail.com>
2025-09-04 18:39:23 +0300

    stable version_1

---
+++ lib/doc_app.dart
@@ -0,0 +1,24 @@
+import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        theme: ThemeData(primaryColor: ColorsManager.mainBlue),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
