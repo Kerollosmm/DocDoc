82231d7f2427faa6b33f388493f7edab568001f7
Kerollosmm <kerollosmilad21@gmail.com>
2025-09-07 01:11:38 +0300

    Implement login screen UI and reusable widgets
    Added the login screen UI with form fields, password visibility toggle, and custom text/button widgets. Introduced reusable AppTextButton and AppTextFormField components, spacing helpers, and terms/conditions and account text widgets for improved consistency and maintainability.

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,19 +1,94 @@
+import 'package:doc_app/core/helpers/spacing.dart';
+import 'package:doc_app/core/theming/colors.dart';
 import 'package:doc_app/core/theming/styles.dart';
+import 'package:doc_app/core/widgets/app_text_button.dart';
+import 'package:doc_app/core/widgets/app_text_form_field.dart';
+import 'package:doc_app/features/login/ui/widgets/terms_and_conditions_text.dart';
+import 'package:doc_app/features/sign_up/ui/widgets/already_have_account_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool abscureText = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Center(
-            child: Text("Login Screen", style: TextStyles.font13DarkBlueMedium),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50.h),
+                Text(
+                  'Welcome Back',
+                  style: TextStyles.font24BlueBold.copyWith(fontSize: 34),
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(hintText: 'Email'),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: 'Password',
+                        obscureText: abscureText,
+                        suffixIcon: GestureDetector(
+                          child: Icon(
+                            abscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: ColorsManager.mainBlue,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              abscureText = !abscureText;
+                            });
+                          },
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: 'Login',
+                        textStyle: TextStyles.font16WhiteMedium,
+                        onPressed: () {},
+                      ),
+
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const AlreadyHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
